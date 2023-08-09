9b9294e55798102c449342f84bf4af54bff0caf2
Abraham Vallejos <abraham-rat@outlook.es>
2023-08-08 21:32:25 -0500

    [Abraham]: props and communicate component father and son.

--- src/FirstComponent.jsx
+++ src/FirstComponent.jsx
@@ -1,13 +1,16 @@
+import PropTypes from 'prop-types'
+
+
 const string = 'es String'
 const number = 23456
-const array = [' String',' String2',' String3']
+const array = [' String', ' String2', ' String3']
 const boolean = false
 const funcion = () => 1 + 1
 const objeto = {nombre: 'Curso de java', duracion: 4}
-const fecha= new Date()
+const fecha = new Date()
 
 
-function FirstComponent() {
+function FirstComponent({title, subtitle, numberprop}) {
     return (
         <>
             <h1>Variables en JSX</h1>
@@ -18,10 +21,27 @@ function FirstComponent() {
             <h2>Tipo funcion(): {funcion()}</h2>
             <h2>Tipo objeto: {JSON.stringify(objeto)}</h2>
             <h2>Tipo fecha: {JSON.stringify(fecha)}</h2>
+            <h1>Props</h1>
+            <h2>Titulo: {title}</h2>
+            <h2>Sub titulo: {subtitle}</h2>
+            <h2>Numberprop: {numberprop + 1}</h2>
+
         </>
 
 
     )
 }
 
+FirstComponent.PropTypes = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string,
+}
+
+
+FirstComponent.defaultProps = {
+    title : "valor por defecto",
+    subtitle : "valor por defecto",
+    numberprop : 10,
+}
 export default FirstComponent;
+
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import FirstComponent from "./FirstComponent.jsx";
 import './styles.css'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstComponent></FirstComponent>
+        <FirstComponent title='va la prop' subtitle='subtitle' numberprop={4}></FirstComponent>
     </React.StrictMode>,
 )
