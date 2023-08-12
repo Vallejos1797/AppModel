53d9745725afd452cb175fed7d35e40d5d6bc6ec
Abraham Vallejos <abraham-rat@outlook.es>
2023-08-12 16:01:25 -0500

    [Abraham]:Implement boostrap in project.

---
+++ src/HooksApp.jsx
@@ -0,0 +1,5 @@
+export const HooksApp = () => {
+    return (<>
+    <h1>Appp Hooks</h1>
+    </>)
+}
--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,24 @@ import CountApp from "./CountApp.jsx";
 import ListApp from "./ListApp.jsx";
 import './styles.css'
 import UserApp from "./componenst/UserApp.jsx";
+import {HooksApp} from "./HooksApp.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstrap
+import 'bootstrap'; // Importa los scripts de Bootstrap
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstComponent title='va la prop' subtitle='subtitle' numberprop={4}></FirstComponent>
-        <CountApp value={0}> </CountApp>
-        <ListApp></ListApp>
-        <h1>useEffect</h1>
-        <UserApp></UserApp>
+        {/*<FirstComponent title='va la prop' subtitle='subtitle' numberprop={4}></FirstComponent>*/}
+        {/*<CountApp value={0}> </CountApp>*/}
+        {/*<ListApp></ListApp>*/}
+        {/*<h1>useEffect</h1>*/}
+        {/*<UserApp></UserApp>*/}
+        <HooksApp></HooksApp>
+        <button className="btn btn-primary">Botón Primario</button>
+        <button className="btn btn-secondary">Botón Secundario</button>
+        <button className="btn btn-success">Botón de Éxito</button>
+        <button className="btn btn-danger">Botón de Peligro</button>
+        <button className="btn btn-warning">Botón de Advertencia</button>
+        <button className="btn btn-info">Botón de Información</button>
+
     </React.StrictMode>,
 )
