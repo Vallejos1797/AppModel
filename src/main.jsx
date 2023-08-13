8509b139609b540400d424a7f345674d66856544
Abraham Vallejos <abraham-rat@outlook.es>
2023-08-12 22:01:25 -0500

    [Abraham]:Create hook custom form base.

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import UserApp from "./componenst/UserApp.jsx";
 import {HooksApp} from "./HooksApp.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstrap
 import 'bootstrap'; // Importa los scripts de Bootstrap
+import {FormComponent} from "./componenst/FormComponent.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
@@ -16,6 +17,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         {/*<ListApp></ListApp>*/}
         {/*<h1>useEffect</h1>*/}
         {/*<UserApp></UserApp>*/}
-        <HooksApp></HooksApp>
+        {/*<HooksApp></HooksApp>*/}
+        <FormComponent>
+
+        </FormComponent>
     </React.StrictMode>,
 )
